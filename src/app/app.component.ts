e3df93324ccb8e02c787fcf9e4407c008f0235c5
AKSHITA KUKREJA <akshita.kukreja.ext@siemens-healthineers.com>
2023-07-21 17:05:23 +0530

    being called once on init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,24 +8,29 @@ import { TableLazyLoadEvent } from 'primeng/table';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./app.component.css']
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 
 export class AppComponent implements OnInit{
   lastScrollPosition: number = 0;
   dt1: any;
 
-  constructor(private dataService: DataService, private chandeDetection: ChangeDetectorRef) {}
+  // constructor(private dataService: DataService, private chandeDetection: ChangeDetectorRef) {}
+
+  constructor(private dataService: DataService) {}
 
   responseData: any[] = [];
   cols: any[] = [];
   selectedData : any;
-  totalRecords!: number;
   loading: boolean = false;
+  hasloaded: boolean = false;
 
   offset: number = 0;
   limit: number = 20;
+
+  lastRowOffset : number = 0 ;
+  totalRecords: number = 0;
 
   ngOnInit() {
 
@@ -43,13 +48,12 @@ export class AppComponent implements OnInit{
       // Add more column definitions here
     ];
 
-      // this.dataService.getData(0,10).pipe(
-      //   tap(response => {
-      //     this.responseData = response.users;
-      //   })         
-      // ).subscribe(response => {
-      //   console.log(this.responseData);
-      // });
+      this.dataService.getData(0,20).subscribe(response => {
+        this.responseData = response.users;
+        this.totalRecords = this.responseData.length;
+        console.log(this.responseData);
+        this.offset += this.limit;   
+      });
 
       // The tap operator can be used at any point within an Observable pipeline.
       //  It does not modify the stream, so it returns the same Observable that it was called on, 
@@ -57,53 +61,66 @@ export class AppComponent implements OnInit{
 
   }
   
+  // onVirtualScroll(event: any){
 
-  loadMoreData(event: any) {
+  //   // const firstRowOfCurrentPage = (this.currentPage - 1) * limit;
+  //   // const lastRowOfCurrentPage = this.currentPage * limit;
+  //   // const lastRowOffset = event.first + event.rows;
+  
+  //   // if (lastRowOffset >= lastRowOfCurrentPage)
+  //       this.lastRowOffset = event.first + event.rows;
+  //       this.totalRecords = this.responseData.length;
 
-    this.loading = true;
-   
-      this.dataService.getData(this.offset, this.limit).pipe(tap((response) => {
-        this.responseData = [...this.responseData, ...response.users];
-        // console.log(this.responseData);
-      })).subscribe(response => {
-        this.loading = false;
-        this.offset += this.limit;
-      });
 
-      //event.forceUpdate();   
+  //   // This condition checks if the last row's offset matches the total number of records
+  //   // indicating that the last row is visible, and we need to load more data to continue scrolling.
+
+  //   if(this.lastRowOffset === this.totalRecords){
     
-    
-  }
+  //     this.dataService.getData(this.offset, this.limit).pipe(tap((response) => {
+  //       this.responseData = [...this.responseData, ...response.users];
+
+  //       this.offset += this.limit;        
+
+  //       //this.responseData.splice(0, this.responseData.length, ...response.users);
+  //       //this.responseData.splice(this.responseData.length,0, ...response.users);
+
+
+  //       //let currentData = this.responseData.slice(event.first, event.first + event.rows);
+  //       //Array.prototype.splice.apply(this.responseData, [...[event.first, event.rows], ...currentData]);
+
+  //       //Array.prototype.splice.apply(this.responseData,  [this.responseData.length,0,...response.users]);
+  //       console.log(this.responseData);
+  //       //event.forceUpdate();
+  //       //this.chandeDetection.detectChanges();
+  //     })).subscribe(response => {
+
+  //       this.loading = false
+  //     });
+  //   }
+  // }
+
+
 
   onVirtualScroll(event: any){
-    const lastRowOffset = event.first + event.rows;
-    const totalRecords = this.responseData.length;
-
-    console.log(lastRowOffset);
-    console.log(totalRecords);
+   
+    // const firstRowOfCurrentPage = (this.currentPage - 1) * limit;
+    // const lastRowOfCurrentPage = this.currentPage * limit;
+    // const lastRowOffset = event.first + event.rows;
+  
+    // if (lastRowOffset >= lastRowOfCurrentPage)
+        this.lastRowOffset = event.first + event.rows;
+        this.totalRecords = this.responseData.length;
 
     // This condition checks if the last row's offset matches the total number of records
     // indicating that the last row is visible, and we need to load more data to continue scrolling.
 
-    if(lastRowOffset === totalRecords){
-    
-      this.dataService.getData(this.offset, this.limit).pipe(tap((response) => {
+    if(this.lastRowOffset === this.totalRecords && this.responseData.length>0){
+      this.dataService.getData(this.offset, this.limit).subscribe(response => {
         this.responseData = [...this.responseData, ...response.users];
-        //this.responseData.splice(0, this.responseData.length, ...response.users);
-        //this.responseData.splice(this.responseData.length,0, ...response.users);
-
-
-        //let currentData = this.responseData.slice(event.first, event.first + event.rows);
-        //Array.prototype.splice.apply(this.responseData, [...[event.first, event.rows], ...currentData]);
-
-        //Array.prototype.splice.apply(this.responseData,  [this.responseData.length,0,...response.users]);
-        console.log(this.responseData);
-        event.forceUpdate();
-        this.chandeDetection.detectChanges();
-      })).subscribe(response => {
+        this.offset += this.limit;        
         this.loading = false;
-        this.offset += this.limit;
-        //this.chandeDetection.detectChanges();
+        this.hasloaded = true;
       });
     }
   }
